07ee8244a2b503daf8f0f290d1c3d9e79c836d99
Kamil Václavek <118657567+vaclavek-kamil@users.noreply.github.com>
2024-07-02 18:21:20 +0200

    build

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const simpleGit = require("simple-git");
 const git = simpleGit();
 const { execSync } = require("child_process");
 
+const defaultSource = "main";
 const productionBranches = ["p1", "p2"];
 const testBranches = ["t1", "t2"];
 const pathsToIgnore = [
@@ -15,30 +16,18 @@ const pathsToIgnore = [
 
 const args = process.argv.slice(2);
 
-let source = args.find((arg) => arg.startsWith("--source="));
-if (!source) {
-  source = "main";
-}
+const source =
+  args.find((arg) => arg.startsWith("--source="))?.split("=")[1] ||
+  defaultSource;
 const production = args.find((arg) => arg.startsWith("--production"))
   ? true
   : false;
 
-const sourceBranchName = source.split("=")[1];
 const branches = production ? productionBranches : testBranches;
 
 async function branchExists(branch) {
-  const branches = await git.branch(["--list", branch]);
-  return branches.all.includes(branch);
-}
-
-async function createBranch(branch, sourceBranch) {
-  try {
-    await git.checkoutBranch(branch, sourceBranch);
-    console.log(`Created and checked out to new branch ${branch}`);
-  } catch (err) {
-    console.error(`Error creating branch ${branch}: ${err.message}`);
-    process.exit(1);
-  }
+  const branchSummary = await git.branch();
+  return branchSummary.all.includes(branch);
 }
 
 async function mergeBranches() {
@@ -47,25 +36,24 @@ async function mergeBranches() {
     await git.fetch();
 
     for (const branch of branches) {
-      // Check if the target branch exists
+      // Check if the branch exists, create it if it doesn't
       const exists = await branchExists(branch);
       if (!exists) {
-        // Create the target branch from the source branch if it doesn't exist
-        await createBranch(branch, sourceBranchName);
+        await git.checkoutLocalBranch(branch);
+        console.log(`Created and checked out to new branch ${branch}`);
       } else {
-        // Checkout the target branch if it exists
         await git.checkout(branch);
         console.log(`Checked out to branch ${branch}`);
       }
 
       // Merge the source branch into the target branch
       try {
-        // Use the "ours" strategy for specified paths
+        // Use the "ours" strategy for templates directory and config.js file
         await git.raw([
           "merge",
           "--strategy=ours",
           "--no-commit",
-          sourceBranchName,
+          source,
           "--",
           ...pathsToIgnore,
         ]);
@@ -74,8 +62,8 @@ async function mergeBranches() {
         );
 
         // Merge the rest of the changes normally
-        await git.merge([sourceBranchName, "--no-ff"]);
-        console.log(`Merged ${sourceBranchName} into ${branch}`);
+        await git.merge([source, "--no-ff"]);
+        console.log(`Merged ${source} into ${branch}`);
       } catch (mergeError) {
         console.error(`Error during merge: ${mergeError.message}`);
         // Abort the merge in case of conflict or error
